54bb58258e9b31576d5f9dd4fa3225a7cce1eefe
Beepered <bktrieu@gmail.com>
2024-10-07 12:13:57 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button"); // createElement(the HTML name of element)
+button.innerHTML = "🐊";
+app.append(button);
