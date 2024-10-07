7ecbd37a12b29f0b40d31e644dc49bf01e7dcc30
Beepered <bktrieu@gmail.com>
2024-10-07 12:32:28 -0700

    (ignore) Small change to see if github pages is working

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ let counter: number = 0;
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game 2 again";
+const gameName = "My game 2 again (2)";
 document.title = gameName;
 
 const header = document.createElement("h1");
